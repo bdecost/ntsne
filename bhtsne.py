9db7b85a8a7569883d4f52084ee09cb267c1a884
Brian DeCost <bdecost@andrew.cmu.edu>
2016-10-20 14:07:12 -0400

    initial commit: rough version requiring bh_tsne executable in working directory.

---
+++ bhtsne.py
@@ -0,0 +1,65 @@
+""" bhtsne.py
+    numpy wrapper for bh_tsne (https://github.com/lvdmaaten/bhtsne)
+    Brian DeCost bdecost@andrew.cmu.edu
+"""
+
+import re
+import struct
+import subprocess
+import numpy as np
+
+# vdM's bh_tsne reads and writes from/to these hardcoded paths
+# in the directory from which bh_tsne is run
+DATAFILE = 'data.dat'
+RESULTFILE = 'result.dat'
+
+def write_tsne_input(X, theta=0.5, perplexity=30, map_dims=2, max_iter=1000, seed=None):
+    """ serialize 2D data matrix (numpy array) with t-SNE options to vdM's binary input format """
+    with open(DATAFILE, 'wb') as f:
+        n, d = X.shape
+        f.write(struct.pack('=i', n))   # number of instances
+        f.write(struct.pack('=i', d))   # initial dimensionality
+        f.write(struct.pack('=d', theta))
+        f.write(struct.pack('=d', perplexity))
+        f.write(struct.pack('=i', map_dims))
+        f.write(struct.pack('=i', max_iter))
+        f.write(X.tobytes())
+
+        if seed is not None:
+            f.write(struct.pack('=i', map_dims))
+            
+def read_tsne_results():
+    """ load t-SNE results from vdM's binary results file format """
+    with open(RESULTFILE, 'rb') as f:
+        n, = struct.unpack('=i', f.read(4))
+        md, = struct.unpack('=i', f.read(4))
+        sz = struct.calcsize('=d')
+        x_tsne = [xx[0] for xx in struct.iter_unpack('=d', f.read(sz*n*md))]
+        
+    x_tsne = np.array(x_tsne).reshape((n,md))
+    return x_tsne
+
+def bh_tsne(X, perplexity=30, theta=0.5):
+    write_tsne_input(X, perplexity=perplexity, theta=theta)
+    subprocess.call('./bh_tsne')
+    return read_tsne_results()
+
+def tsne_error(results):
+    """ find the error string for each iteration; get the min (likely the last iteration...) """
+    errorstrings = re.findall('error is \d+\.\d+', results.decode())
+    # lexicographic ordering is equivalent to splitting each sting and converting to float...
+    error = min(errorstrings).split()[-1]
+    return float(error)
+
+def best_bh_tsne(X, perplexity=30, theta=0.5, n_iter=10):
+    write_tsne_input(X, perplexity=perplexity, theta=theta)
+    lowest_error = 1e9
+    x_tsne = None
+    for iteration in range(n_iter):
+        results = subprocess.check_output('./bh_tsne')
+        error = tsne_error(results)
+        print('error is {}'.format(error))
+        if error < lowest_error:
+            lowest_error = error
+            x_tsne = read_tsne_results()
+    return x_tsne
