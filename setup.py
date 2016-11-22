6fa2b41bbf756932e843a0b5ba199ab7afe2399c
Brian DeCost <bdecost@andrew.cmu.edu>
2016-11-22 11:14:27 -0500

    add some details to setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 setup(
     name='ntsne',
+    author='Brian DeCost',
+    author_email='bdecost@andrew.cmu.edu',
     version='0.1',
+    description='A simple numpy wrapper around bh_tsne.',
     py_modules=['ntsne'],
     install_requires=[
         'numpy',
